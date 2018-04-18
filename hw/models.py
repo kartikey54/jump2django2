1d6413b675e1466f37b837a6aa3d9f80cbd319a1
K <sharmakartikey54@gmail.com>
2018-04-18 13:59:08 +0530

    models made

--- hw/models.py
+++ hw/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Comment(models.Model):
+    name = models.CharField(max_length = 1200)
+    comment = models.TextField()
+    date = models.DateTimeField(default = timezone.now)
+    def __str__(self):
+        return self.name
