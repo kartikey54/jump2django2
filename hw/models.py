924fee9d09de277fa608682017e18f7117519481
K <sharmakartikey54@gmail.com>
2018-04-18 14:30:22 +0530

    forms_models_attached

--- hw/models.py
+++ hw/models.py
@@ -6,4 +6,4 @@ class Comment(models.Model):
     comment = models.TextField()
     date = models.DateTimeField(default = timezone.now)
     def __str__(self):
-        return self.name
+        return self.name + self.comment
--- hw/views.py
+++ hw/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 # Create your views here.
+from .forms import CommentForm
+from .models import Comment
 
 def index(request):
-    return render(request, 'hw/index.html')
+    comments = Comment.objects.order_by('-date')
+    context = {'comments' : comments}
+    return render(request, 'hw/index.html', context = context)
 
 def sign(request):
-    return render(request, 'hw/sign.html')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = Comment(name=request.POST['name'],
+                                comment = request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
+    context = {'form' : form}
+    return render(request, 'hw/sign.html', context = context)
